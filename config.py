2ac33ff66e52852d4e574c776354aedc9d4abf0d
SBKofficial <86524089+SBKofficial@users.noreply.github.com>
2026-02-12 14:21:24 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,4 +6,4 @@ load_dotenv()
 class Config:
     BOT_TOKEN = os.getenv("BOT_TOKEN")
     MONGO_URI = os.getenv("MONGO_URI")
-    [span_1](start_span)ADMIN_IDS = [5242138546]  # From your original bot.py[span_1](end_span)
+    ADMIN_IDS = [5242138546]
